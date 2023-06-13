83f50b316aaa21b217c19ab0273a65bf29cf9ed9
mewluee <yunide073@gmail.com>
2023-06-13 16:20:35 +0900

    init

---
+++ hn/src/main/java/com/example/hn/HnApplication.java
@@ -0,0 +1,14 @@
+package com.example.hn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HnApplication.class, args);
+
+    }
+
+}
